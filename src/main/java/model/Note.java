0530bee3e2722b928dce87db4878300fb17a56b8
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-04-26 15:11:26 +0300

    Skeleton classes

---
+++ src/main/java/model/Note.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Note {
+
+    private String name;
+    private String path;
+    private List<Keyword> keywords;
+
+    public Note(String name, String path) {
+        this.name = name;
+        this.path = path;
+        keywords = new LinkedList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public List<Keyword> getKeywords() {
+        return keywords;
+    }
+
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,51 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.Hashtable;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
+
+    private Hashtable<String,Note> notes;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void addNote(Note n) {
+
+    }
+}
