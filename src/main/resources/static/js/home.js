5d3722e118796c0965ccc14c2fca13b37014c0e6
Toan Nguyen <toan.nguyen@frosmo.com>
2018-05-08 05:44:14 +0300

    Add additionalCSS and JS for homepage template

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,6 @@
+(function() {
+    var notes = $('.btn.note.grow');
+    if (notes.length === 0) {
+       $('.notes-heading').text('You could create a note using the new icon in the left navigation bar.');
+    }
+}())
